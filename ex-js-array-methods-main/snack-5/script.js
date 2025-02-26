5cb37c5b696d91827f814d649b5eb8eda4138259
EduardCancel <eduardcancel.boolean@gmail.com>
2025-02-26 14:58:33 +0100

    complete snack-5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -1,4 +1,12 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
+const numbEven = nums.filter((even) =>{
+    if( even % 2 === 0){
+        return true
+    }
+    return false
+})
+console.log(numbEven);
+
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
